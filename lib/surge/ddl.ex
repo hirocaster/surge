46a17d30188d2daf17ea48e8e53f005e0cfd5e41
hirocaster <hohtsuka@gmail.com>
2016-11-24 18:33:50 +0900

    Refactor generate update table request at global index updates

--- lib/surge/ddl.ex
+++ lib/surge/ddl.ex
@@ -30,99 +30,112 @@ defmodule Surge.DDL do
   end
 
   def update_table(model) do
-    table_name            = model.__table_name__
     keys                  = model.__keys__
     secondary_keys        = model.__secondary_keys__
     global_keys           = model.__global_keys__
     attribute_definitions = pk_spec(keys) ++ secondary_keys ++ global_keys
     [read, write]         = model.__throughput__
+    global_indexes        = model.__global_indexes__
+    describe_table        = describe_table(model)
 
     attributes = %{}
+    |> update_attribute_defnitions(attribute_definitions, describe_table)
+    |> update_provisioned_throughput([read, write], describe_table)
+    |> update_global_secondary_indexes(global_indexes, describe_table)
 
-    eixist_attribute_definitions = describe_table(model)["AttributeDefinitions"]
-    exisst_attribute_names = Enum.map(eixist_attribute_definitions, &(String.to_atom(&1["AttributeName"]))) # [:id, :time]
-
-    update_attr = Enum.reject(attribute_definitions, fn({key, _}) -> Enum.member?(exisst_attribute_names, key) end)
-
-    if Enum.count(update_attr) > 0 do
-      update_attributes = %{
-        "AttributeDefinitions" => update_attr |> encode_key_definitions,
-      }
-      attributes = Map.merge(attributes, update_attributes)
-    end
-
-
-    table_info = describe_table(model)
-    exist_read  = Map.get(table_info["ProvisionedThroughput"], "ReadCapacityUnits")
-    exist_write = Map.get(table_info["ProvisionedThroughput"], "WriteCapacityUnits")
-
-    unless [exist_read, exist_write] == [read, write] do
-      update_previsioned_throughput = %{"ProvisionedThroughput" => %{
-                                       "ReadCapacityUnits"  => read,
-                                       "WriteCapacityUnits" => write
-                                     }}
-      attributes = Map.merge(attributes, update_previsioned_throughput)
-    end
-
-
-    global_indexes = model.__global_indexes__
-
-    case describe_table(model)["GlobalSecondaryIndexes"] do
-      nil ->
-        if Enum.count(global_indexes) > 0 do
-
-          indexes = for global_index <- global_indexes do
-            %{"Create" => global_index}
-          end
-
-          global_secondary_index_updates = %{
-            "GlobalSecondaryIndexUpdates" => indexes
-          }
-          attributes = Map.merge(attributes, global_secondary_index_updates)
-        else
-          nil
-        end
-
-      index_of_list when is_list(index_of_list) ->
-
-        # diff create(model - db)
-        create_indexes = diff_indexes(global_indexes, Surge.Util.camel_array_map_to_snake_array_map(index_of_list))
-
-        indexes = for global_index <- create_indexes do
-          %{"Create" => global_index}
-        end
-        global_secondary_index_updates = %{
-          "GlobalSecondaryIndexUpdates" => indexes
-        }
-        attributes = Map.merge(attributes, global_secondary_index_updates)
-
-        # diff delete index (db - model)
-        delete_indexes = index_of_list
-        |> Surge.Util.camel_array_map_to_snake_array_map
-        |> diff_indexes(global_indexes)
-
-        if Enum.count(delete_indexes) > 0 do
-          indexes = for global_index <- delete_indexes do
-            %{"Delete" => global_index}
-          end
-          index_updates = %{
-            "GlobalSecondaryIndexUpdates" => indexes
-          }
-          attributes = Map.merge(attributes, index_updates)
-        end
-    end
-
-    table_name
+    model.__table_name__
     |> ExAws.Dynamo.update_table(attributes)
     |> ExAws.request
   end
 
-  defp diff_indexes(base_indexes, subtraction_indexes) do
+  defp update_provisioned_throughput(request_body, [read, write], describe_table) do
+    exists_read  = Map.get(describe_table["ProvisionedThroughput"], "ReadCapacityUnits")
+    exists_write = Map.get(describe_table["ProvisionedThroughput"], "WriteCapacityUnits")
+
+    if [exists_read, exists_write] == [read, write] do
+      request_body
+    else
+      update_previsioned_throughput = %{"ProvisionedThroughput" => %{
+                                       "ReadCapacityUnits"  => read,
+                                       "WriteCapacityUnits" => write
+                                     }}
+      Map.merge(request_body, update_previsioned_throughput)
+    end
+  end
+
+  defp update_attribute_defnitions(request_body, attribute_definitions, describe_table) do
+    add_attributes_defnitions = Enum.reject(attribute_definitions, fn({key, _}) -> Enum.member?(exsists_attribute_names(describe_table), key) end)
+
+    if Enum.count(add_attributes_defnitions) > 0 do
+      update_attributes = %{
+        "AttributeDefinitions" => add_attributes_defnitions |> encode_key_definitions,
+      }
+
+      Map.merge(request_body, update_attributes)
+    else
+      request_body
+    end
+  end
+
+  defp exsists_attribute_names(describe_table) do
+    describe_table["AttributeDefinitions"]
+    |> Enum.map(&(String.to_atom(&1["AttributeName"]))) # => [:id, :time]
+  end
+
+  defp update_global_secondary_indexes(request_body, global_indexes, describe_table) do
+    case describe_table["GlobalSecondaryIndexes"] do
+      nil ->
+        global_indexes_update_body(request_body, create_index_body_in_global_indexes_update(global_indexes))
+
+      exists_indexes_by_describe_table when is_list(exists_indexes_by_describe_table) ->
+        exists_indexes = exists_indexes_by_describe_table
+        |> Surge.Util.camel_array_map_to_snake_array_map
+
+        # diff create(model - db)
+        create_indexes_body = global_indexes
+        |> subtraction_index(exists_indexes)
+        |> create_index_body_in_global_indexes_update
+
+        # diff delete index (db - model)
+        delete_indexes_body = exists_indexes
+        |> subtraction_index(global_indexes)
+        |> delete_index_body_in_global_indexes_update
+
+        request_body
+        |> global_indexes_update_body(create_indexes_body)
+        |> global_indexes_update_body(delete_indexes_body)
+    end
+  end
+
+  defp subtraction_index(base_indexes, subtraction_indexes) do
     Enum.reject(base_indexes, fn(exists_index) ->
-      Enum.find(subtraction_indexes, nil, fn(subtraction_index) ->
-        subtraction_index.index_name == exists_index.index_name
+      Enum.find(subtraction_indexes, nil, fn(sub_index) ->
+        sub_index.index_name == exists_index.index_name
       end)
     end)
+  end
+
+  defp global_indexes_update_body(request_body, indexes_updates) do
+    if Enum.count(indexes_updates) > 0 do
+      global_secondary_index_updates = %{
+        "GlobalSecondaryIndexUpdates" => indexes_updates
+      }
+      Map.merge(request_body, global_secondary_index_updates)
+    else
+      request_body
+    end
+  end
+
+  defp create_index_body_in_global_indexes_update(global_indexes) do
+    for index <- global_indexes do
+      %{"Create" => index}
+    end
+  end
+
+  defp delete_index_body_in_global_indexes_update(global_indexes) do
+    for index <- global_indexes do
+      %{"Delete" => index}
+    end
   end
 
   def delete_table(model) do
